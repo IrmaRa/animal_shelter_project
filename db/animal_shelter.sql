716dc7b674a6cca1348a851b1bab77161c8ee816
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-05-05 15:30:59 +0100

    Create animal_shelter database and add owners and animals tables

---
+++ db/animal_shelter.sql
@@ -0,0 +1,19 @@
+DROP TABLE animals;
+DROP TABLE owners;
+
+
+CREATE TABLE owners (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE animals (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  age INT2,
+  image_url VARCHAR(255),
+  admission_date DATE,
+  adoption_case BOOLEAN,
+  owner_id INT4 REFERENCES owners(id)
+);
---
+++ db/sql_runner.rb
@@ -0,0 +1,19 @@
+require('pg')
+
+class SqlRunner
+
+  def SqlRunner.run(sql)
+
+    begin
+      db = PG.connect({ dbname: 'animal_shelter', 
+        host: 'localhost' })
+      result = db.exec(sql)
+    ensure
+
+      db.close()
+    end  
+
+    return result
+  end
+  
+end
