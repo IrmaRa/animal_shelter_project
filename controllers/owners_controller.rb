ec048522063ed21a07386c10f9b3b371d03c8993
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-05-08 21:10:23 +0100

    Add edit option

--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -12,3 +12,19 @@ get '/owners/new' do
   @owners = Owner.all()
   erb(:"owners/new")
 end
+
+post '/owners' do
+  @owner = Owner.new(params)
+  @owner.save()
+  erb(:"owners/create")
+end
+
+get '/owners/:id' do
+  @owner = Owner.find(params[:id])
+  erb(:"owners/index")
+end
+
+get '/owners/:id/edit' do
+  @owner = Owner.find(params[:id])
+  erb(:"owners/edit")
+end
