5a56489115d048637f96f236993f5b5b99a66b64
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-05-08 20:07:38 +0100

    Add owners index view

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('controllers/animals_controller')
+require_relative('controllers/owners_controller')
 
 get '/animals' do
   @animals = Animal.all()
--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -3,3 +3,7 @@ require('sinatra/contrib/all')
 require_relative('../models/animal')
 require_relative('../models/owner')
 
+get '/owners' do
+  @owners = Owner.all()
+  erb(:"owners/index")
+end
